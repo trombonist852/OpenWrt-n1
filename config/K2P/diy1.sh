2c3574abed8b737d7af8a69df2ccbd3d0515c9ce
trombonist852 <trombonist852@gmail.com>
2021-03-09 14:13:59 +0800

    Delete config/K2P directory

--- config/K2P/diy1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# 下载主题
-svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
--- config/K2P/diy2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 替换默认IP
-sed -i 's#192.168.1.1#192.168.1.2#g' package/base-files/files/bin/config_generate
-# 替换默认主题
-sed -i 's#luci-theme-bootstrap#luci-theme-infinityfreedom#g' feeds/luci/collections/luci/Makefile
